836586bc52af7f3bdb4775f1327459780a9c043d
Mihai Cozma <mihai.cozma@savatech.ro>
2016-08-29 15:49:40 +0300

    Tutorial 8 - Widget Dialogs http://localhost:8090/aikau-sample/page/ap/ws/users-and-groups

--- src/main/webapp/WEB-INF/webscripts/pages/users-and-groups.get.js
+++ src/main/webapp/WEB-INF/webscripts/pages/users-and-groups.get.js
@@ -1,8 +1,27 @@
+var options = [];
+var result = remote.call("/api/people?filter=");
+if (result.status.code == status.STATUS_OK)
+{
+   var rawData = JSON.parse(result);
+   if (rawData && rawData.people)
+   {
+      var people = rawData.people;
+      for (var i = 0; i < people.length; i++)
+      {
+         options.push(
+         {
+            value : people[i].userName,
+            label : people[i].firstName + " " + people[i].lastName
+         });
+      }
+   }
+}
+
 model.jsonModel =
 {
    services : [ "alfresco/services/CrudService",
-         "alfresco/services/DialogService",
-         "tutorial/UserAndGroupService"],
+         "alfresco/services/DialogService", "tutorial/UserAndGroupService",
+         "alfresco/services/OptionsService" ],
    widgets : [
          {
             name : "alfresco/lists/AlfList",
@@ -31,10 +50,110 @@ model.jsonModel =
                               {
                                  widgets : [
                                  {
-                                    name : "alfresco/renderers/Property",
+                                    name : "alfresco/renderers/PropertyLink",
                                     config :
                                     {
-                                       propertyToRender : "displayName"
+                                       propertyToRender : "displayName",
+                                       useCurrentItemAsPayload : false,
+                                       publishTopic : "ALF_CREATE_DIALOG_REQUEST",
+                                       publishPayloadType : "PROCESS",
+                                       publishPayloadModifiers : [ "processCurrentItemTokens" ],
+                                       publishPayload :
+                                       {
+                                          dialogTitle : "{displayName}",
+                                          fixedWidth : true,
+                                          widgetsContent : [
+                                                {
+                                                   name : "alfresco/forms/Form",
+                                                   config :
+                                                   {
+                                                      okButtonLabel : "Add User",
+                                                      okButtonPublishTopic : "TUTORIAL_ADD_USER_TO_GROUP",
+                                                      //generatePubSubScope : true,
+                                                      // or pubSubScope:
+                                                      // "SOME_SCOPE_VALUE",
+                                                      okButtonPublishPayload :
+                                                      {
+                                                         groupId : "{shortName}",
+                                                         pubSubScope : "GROUP_USERS_"
+                                                      },
+                                                      okButtonPublishGlobal : true,
+                                                      showCancelButton : false,
+                                                      widgets : [
+                                                      {
+                                                         name : "alfresco/forms/controls/Select",
+                                                         config :
+                                                         {
+                                                            label : "User",
+                                                            description : "Select a user to add to the group",
+                                                            name : "userName",
+                                                            optionsConfig :
+                                                            {
+                                                               // publishTopic :
+                                                               // "ALF_GET_FORM_CONTROL_OPTIONS",
+                                                               fixed : options
+                                                            // publishPayload: {
+                                                            // url: url.context
+                                                            // +
+                                                            // "/proxy/alfresco/api/people?filter=",
+                                                            // itemsAttribute:
+                                                            // "people",
+                                                            // labelAttribute:
+                                                            // "userName",
+                                                            // valueAttribute:
+                                                            // "userName"
+                                                            // }
+                                                            // }
+                                                            }
+                                                         }
+                                                      } ]
+                                                   }
+                                                },
+                                                {
+                                                   name : "alfresco/lists/AlfList",
+                                                   config :
+                                                   {
+                                                      waitForPageWidgets : false,
+                                                      //generatePubSubScope : true,
+                                                      pubSubScope: "GROUP_USERS_",
+                                                      loadDataPublishTopic : "ALF_CRUD_GET_ALL",
+                                                      loadDataPublishPayload :
+                                                      {
+                                                         url : "api/groups/{shortName}/children?sortBy=displayName&maxItems=50&skipCount=0"
+                                                      },
+                                                      itemsProperty : "data",
+                                                      widgets : [
+                                                      {
+                                                         name : "alfresco/documentlibrary/views/AlfDocumentListView",
+                                                         config :
+                                                         {
+                                                            widgets : [
+                                                            {
+                                                               name : "alfresco/lists/views/layouts/Row",
+                                                               config :
+                                                               {
+                                                                  widgets : [
+                                                                  {
+                                                                     name : "alfresco/lists/views/layouts/Cell",
+                                                                     config :
+                                                                     {
+                                                                        widgets : [
+                                                                        {
+                                                                           name : "alfresco/renderers/Property",
+                                                                           config :
+                                                                           {
+                                                                              propertyToRender : "displayName"
+                                                                           }
+                                                                        } ]
+                                                                     }
+                                                                  } ]
+                                                               }
+                                                            } ]
+                                                         }
+                                                      } ]
+                                                   }
+                                                } ]
+                                       }
                                     }
                                  } ]
                               }
@@ -58,7 +177,7 @@ model.jsonModel =
                   dialogTitle : "Create Group",
                   dialogConfirmationButtonTitle : "Create",
                   dialogCancellationButtonTitle : "Cancel",
-                  formSubmissionTopic: "TUTORIAL_CREATE_GROUP",
+                  formSubmissionTopic : "TUTORIAL_CREATE_GROUP",
                   fixedWidth : true,
                   widgets : [
                         {
--- src/main/webapp/js/tutorial/UserAndGroupService.js
+++ src/main/webapp/js/tutorial/UserAndGroupService.js
@@ -11,21 +11,53 @@ define([ "dojo/_base/declare", "alfresco/core/Core", "dojo/_base/lang",
          lang.mixin(this, args);
          this.alfSubscribe("TUTORIAL_CREATE_GROUP", lang.hitch(this,
                this.createGroup));
+         this.alfSubscribe("TUTORIAL_ADD_USER_TO_GROUP", lang.hitch(this,
+               this.addUserToGroup));
       },
 
-      createGroup: function tutorial_UserAndGroupService__createGroup(payload) {
-         this.serviceXhr({
-           url: AlfConstants.PROXY_URI + "api/rootgroups/" + payload.groupId,
-           method: "POST",
-           data: {
-             displayName: payload.displayName
-           },
-           successCallback: this.onSuccess,
-           callbackScope: this
+      createGroup : function tutorial_UserAndGroupService__createGroup(payload)
+      {
+         this.serviceXhr(
+         {
+            url : AlfConstants.PROXY_URI + "api/rootgroups/" + payload.groupId,
+            method : "POST",
+            data :
+            {
+               displayName : payload.displayName
+            },
+            successCallback : this.onSuccess,
+            callbackScope : this
          });
-       },
-       onSuccess: function tutorial_UserAndGroupService__onSuccess(response, originalRequestConfig) {
-          this.alfPublish("ALF_DOCLIST_RELOAD_DATA", {});
-        }
+      },
+      onSuccess : function tutorial_UserAndGroupService__onSuccess(response,
+            originalRequestConfig)
+      {
+
+         var pubSubScope = lang.getObject("data.pubSubScope", false,
+               originalRequestConfig);
+         if (pubSubScope == null)
+         {
+            pubSubScope = "";
+         }
+         this.alfPublish(pubSubScope + "ALF_DOCLIST_RELOAD_DATA");
+
+         // this.alfPublish("ALF_DOCLIST_RELOAD_DATA", {});
+      },
+      addUserToGroup : function tutorial_UserAndGroupService__addUserToGroup(
+            payload)
+      {
+         this.serviceXhr(
+         {
+            url : AlfConstants.PROXY_URI + "api/groups/" + payload.groupId
+                  + "/children/" + payload.userName,
+            method : "POST",
+            data :
+            {
+               pubSubScope : payload.pubSubScope
+            },
+            successCallback : this.onSuccess,
+            callbackScope : this
+         });
+      },
    });
 });
